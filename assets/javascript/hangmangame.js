0b102912ef7b685e49944dcf9dba29fc7b790965
Brandon <blshort43@gmail.com>
2016-08-12 21:30:45 -0500

    guessed letters replace dashes, removed commas between letters

--- assets/javascript/hangmangame.js
+++ assets/javascript/hangmangame.js
@@ -5,21 +5,86 @@ var wins = 0;
 var losses = 0;
 var words = ['moustache', 'bicycle', 'skinny jeans', 'flannel', 'kombucha', 'fedora'];
 
+var wordArray = []; //holds array of the random word chosen
+var guessArray = []; //creates an array of the user guessed letters
+
+var dashArray = [];//creates an array of dashes for the amount of letters in the randome word chosen
+
+
 
 window.onload = function(){
 	var randomWord = words[Math.floor(Math.random() * words.length)];
-	var wordArray = randomWord.split("");
-	console.log(wordArray);
+	wordArray = randomWord.split("");
+
+
+	console.log(wordArray); //console log the random word chosen & converted to an array
+
 	for (var i=0; i<wordArray.length; i++){
-	var div = document.getElementById('randomWord');
-	div.innerHTML = div.innerHTML + "_" + " ";
+		dashArray.push("_" + " ");
+		var newBlankDashArray = dashArray.join('');
+		document.querySelector('#randomWord').innerHTML = newBlankDashArray;
+
+		console.log(dashArray); //log the blank dash array
+
+
+	window.onkeyup = function(event) {
+		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		guessArray.push(userGuess);
+
+		console.log(userGuess); //log the user key pressed
+		console.log(guessArray); //log the array of user key presses
+
+	for (var i=0; i<wordArray.length; i++){
+		if (userGuess == wordArray[i]){
+			dashArray.splice(i, 1, userGuess);
+			var newDashArray = dashArray.join('');
+			document.querySelector('#randomWord').innerHTML = newDashArray;
+
+			console.log(newDashArray);
+			// var a = dashArray.join();
+			// console.log(a);
+			console.log(dashArray); //log the dash array spliced with a correct letter guessed
+		}
 	}
 }
+}
+}
+
+
+
+
+
+
+
+
+
+
+// 	for (var i=0; i<wordArray.length; i++){
+// 	var div = document.getElementById('randomWord');
+
+    
+//     if (userGuess == wordArray[i]){
+//     	tempArray.splice(i, 1, userGuess);
+// 		div.innerHTML = tempArray;
+// 	}
+
+// 	if (userGuess != wordArray[i]){
+// 		tempArray.splice(i, 1, guessArray[i]);
+// 		div.innerHTML = tempArray;
+// 	}
+
+//     guessArray = tempArray.join("");
+
+// }
+// console.log(guessArray);
+// console.log(tempArray);
+// }
+// }
 
 
 // window.onkeyup = function(event) {
 // 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
+// }
 
 	
 
