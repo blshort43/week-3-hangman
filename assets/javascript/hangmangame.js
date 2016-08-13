bc439e1c6dd737a499fe7a1665b546ebcc2bfe77
Brandon <blshort43@gmail.com>
2016-08-13 16:20:40 -0500

    finished hangman

--- assets/javascript/hangmangame.js
+++ assets/javascript/hangmangame.js
@@ -1,120 +1,125 @@
 
 
-var remainingGuesses = 20;
+var remainingGuesses = 10;
 var wins = 0;
 var losses = 0;
 var words = ['moustache', 'bicycle', 'skinny jeans', 'flannel', 'kombucha', 'fedora'];
-
 var wordArray = []; //holds array of the random word chosen
 var guessArray = []; //creates an array of the user guessed letters
-
 var dashArray = [];//creates an array of dashes for the amount of letters in the randome word chosen
+var userGuess = "";
+var correctLetters = [];
+var newBlankDashArray = [];
+var newDashArray = [];
+var incorrectGuess = [];
+//#arrays4days
 
+var reset = function(){
+				remainingGuesses = 10;
+				wordArray = []; //holds array of the random word chosen
+				guessArray = []; //creates an array of the user guessed letters
+				dashArray = [];//creates an array of dashes for the amount of letters in the randome word chosen
+				userGuess = "";
+				correctLetters = [];
+				window.onload();
+			}
 
+var dashAnswer = function(){
+					var post = newBlankDashArray
+				}
+
+var update = function(){
+				var html = 
+				"<p>Wins: " + wins + "</p>" +
+				"<p>Number of guesses remaining: " + remainingGuesses + "</p>" +
+				"<p>Letters Guessed: " + guessArray + "</p>" +
+				"<p>" + newDashArray + "</p>";
+				document.querySelector("#superDiv").innerHTML = html;
+				}
 
 window.onload = function(){
-	var randomWord = words[Math.floor(Math.random() * words.length)];
-	wordArray = randomWord.split("");
+				var randomWord = words[Math.floor(Math.random() * words.length)];
+				wordArray = randomWord.split("").filter(function(item, pos) {
+    			return randomWord.indexOf(item) == pos;
+				});
+				update();
 
+	counter = 0;
 
 	console.log(wordArray); //console log the random word chosen & converted to an array
 
-	for (var i=0; i<wordArray.length; i++){
-		dashArray.push("_" + " ");
-		var newBlankDashArray = dashArray.join('');
-		document.querySelector('#randomWord').innerHTML = newBlankDashArray;
 
-		console.log(dashArray); //log the blank dash array
+	for (var i=0; i<randomWord.length; i++){
+		dashArray.push("_" + " ");
+		newBlankDashArray = dashArray.join('');
+		newDashArray = newBlankDashArray;
+		// document.querySelector('#randomWord').innerHTML = newBlankDashArray;
+
+		console.log(newBlankDashArray); //log the blank dash array
+		};	
+		update();
 
 
 	window.onkeyup = function(event) {
 		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		guessArray.push(userGuess);
 
-		console.log(userGuess); //log the user key pressed
-		console.log(guessArray); //log the array of user key presses
 
-	for (var i=0; i<wordArray.length; i++){
-		if (userGuess == wordArray[i]){
-			dashArray.splice(i, 1, userGuess);
-			var newDashArray = dashArray.join('');
-			document.querySelector('#randomWord').innerHTML = newDashArray;
 
-			console.log(newDashArray);
-			// var a = dashArray.join();
-			// console.log(a);
-			console.log(dashArray); //log the dash array spliced with a correct letter guessed
+
+
+		if (guessArray.indexOf(userGuess) == -1){
+			guessArray.push(userGuess);
+			update();
+
+			// This code runs when the key is not a duplicate
+			
+			if (wordArray.indexOf(userGuess) == -1){
+				// This code runs when the key is not in the wordArray
+
+				remainingGuesses--;
+				update();
+			} else {
+				correctLetters.push(userGuess);
+
+				// This code runs when the key is in the wordArray
+				for (var i=0; i<randomWord.length; i++){
+					if (userGuess == randomWord[i]){
+						dashArray.splice(i, 1, userGuess);
+						newDashArray = dashArray.join('');
+						update();
+					}
+				}
+
+
+				if (correctLetters.length == wordArray.length){
+					wins++;
+					alert("You won! The word was " + newDashArray.toString());
+					reset();
+					console.log(wins);
+
+				}
+				
+			}
 		}
-	}
-}
-}
-}
+		
+		if (remainingGuesses === 0){
+			reset();
+		}
+		
+
+		
+			console.log(userGuess); //log the user key pressed
+			console.log(guessArray); //log the array of user key presses
+			console.log(correctLetters);
+
+				}
+			}
+
+			// var guessed = "<p>Your Guesses So Far: " + guessArray.join(" ") + "</p>"
+			// document.querySelector('#guessedLetters').innerHTML = guessed;
+			
+		
 
 
 
 
-
-
-
-
-
-
-// 	for (var i=0; i<wordArray.length; i++){
-// 	var div = document.getElementById('randomWord');
-
-    
-//     if (userGuess == wordArray[i]){
-//     	tempArray.splice(i, 1, userGuess);
-// 		div.innerHTML = tempArray;
-// 	}
-
-// 	if (userGuess != wordArray[i]){
-// 		tempArray.splice(i, 1, guessArray[i]);
-// 		div.innerHTML = tempArray;
-// 	}
-
-//     guessArray = tempArray.join("");
-
-// }
-// console.log(guessArray);
-// console.log(tempArray);
-// }
-// }
-
-
-// window.onkeyup = function(event) {
-// 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-// }
-
-	
-
-// 	if (remainingGuesses >= 0){
-// 		remainingGuesses--;
-// 	}
-
-// 	if ((userGuess === computerChoice && remainingGuesses >= 0)){
-// 		wins++;
-// 	}else if ((userGuess !== computerChoice && remainingGuesses >= 0)){
-// 		losses++;
-
-// 		var letter = "<p>Your Guesses So Far: " + userGuess + ",</p>"
-// 		var winNumber = "<p>Wins: " + wins + "</p>";
-// 		var lossNumber = "<p>Losses: " + losses + "</p>";
-// 		var guessLeft = "<p>Guesses Left: " + remainingGuesses + "</p>";
-
-
-// 		// Placing the html into the game ID
-// 		var div = document.getElementById('guessedLetters');
-// 		div.innerHTML = div.innerHTML + userGuess + ", ";
-
-// 		document.querySelector('#wins').innerHTML = winNumber;
-// 		document.querySelector('#losses').innerHTML = lossNumber;
-// 		document.querySelector('#guessesLeft').innerHTML = guessLeft;
-// 	}
-// 	}
-
-
-
-
-
-	
